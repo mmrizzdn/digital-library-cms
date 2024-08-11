c9eb25736dbb5ef8755c98702a68f63c2efd3b6f
mmrizzdn <zoed.eldien15@gmail.com>
2024-08-12 06:23:14 +0700

    init

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 <section class="home" id="home">
-    <!-- Home -->
+    {{-- Home --}}
     <div class="align-middle position-absolute top-50 start-50 translate-middle">
         <h1 class="text-lg-center">Welcome</h1>
         <p class="text-center">
